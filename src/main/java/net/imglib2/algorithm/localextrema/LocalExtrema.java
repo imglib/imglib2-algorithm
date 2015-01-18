80c20bb607c5decccf7473647f8d9c454cab66da
dietzc <christian.dietz@uni-konstanz.de>
2015-01-18 12:06:11 +0100

    LocalExtrema: avoid ArithmeticException (division by zero)
    At the moment, numTasks may be zero (if full.dimension(splitd) = 0,
    which causes a ArithmeticException during the calculation of dsize.

--- src/main/java/net/imglib2/algorithm/localextrema/LocalExtrema.java
+++ src/main/java/net/imglib2/algorithm/localextrema/LocalExtrema.java
@@ -111,7 +111,7 @@ public class LocalExtrema
 		final int splitd = n - 1;
 		// FIXME is there a better way to determine number of threads
 		final int numThreads = Runtime.getRuntime().availableProcessors();
-		final int numTasks = numThreads <= 1 ? 1 : ( int ) Math.min( full.dimension( splitd ), numThreads * 20 );
+		final int numTasks = Math.max( numThreads <= 1 ? 1 : ( int ) Math.min( full.dimension( splitd ), numThreads * 20 ), 1);
 		final long dsize = full.dimension( splitd ) / numTasks;
 		final long[] min = new long[ n ];
 		final long[] max = new long[ n ];
