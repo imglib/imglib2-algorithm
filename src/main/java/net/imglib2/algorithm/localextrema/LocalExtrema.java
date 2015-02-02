4a06b5aeaea7425a51125f3492bc9656dba9aa7e
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2015-02-02 15:47:54 +0100

    simplify numTasks computation in LocalExtrema.

--- src/main/java/net/imglib2/algorithm/localextrema/LocalExtrema.java
+++ src/main/java/net/imglib2/algorithm/localextrema/LocalExtrema.java
@@ -111,7 +111,7 @@ public class LocalExtrema
 		final int splitd = n - 1;
 		// FIXME is there a better way to determine number of threads
 		final int numThreads = Runtime.getRuntime().availableProcessors();
-		final int numTasks = Math.max( numThreads <= 1 ? 1 : ( int ) Math.min( full.dimension( splitd ), numThreads * 20 ), 1);
+		final int numTasks = Math.max( Math.min( ( int ) full.dimension( splitd ), numThreads * 20 ), 1 );
 		final long dsize = full.dimension( splitd ) / numTasks;
 		final long[] min = new long[ n ];
 		final long[] max = new long[ n ];
