33dd74c0803258eaaa61b7f220241c0b9d0f760c
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2015-02-08 19:30:47 +0100

    Use the same static inner classes in RectangleShape and CenteredRectangleShape.
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src/main/java/net/imglib2/algorithm/morphology/neighborhoods/CenteredRectangleShape.java
+++ src/main/java/net/imglib2/algorithm/morphology/neighborhoods/CenteredRectangleShape.java
@@ -10,13 +10,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,7 +28,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are
  * those of the authors and should not be interpreted as representing official
  * policies, either expressed or implied, of any organization.
@@ -37,27 +37,18 @@
 
 package net.imglib2.algorithm.morphology.neighborhoods;
 
-import java.util.Iterator;
-
-import net.imglib2.AbstractEuclideanSpace;
-import net.imglib2.AbstractInterval;
-import net.imglib2.Cursor;
 import net.imglib2.FinalInterval;
-import net.imglib2.FlatIterationOrder;
 import net.imglib2.Interval;
-import net.imglib2.IterableInterval;
-import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.algorithm.region.localneighborhood.Neighborhood;
 import net.imglib2.algorithm.region.localneighborhood.RectangleNeighborhood;
-import net.imglib2.algorithm.region.localneighborhood.RectangleNeighborhoodCursor;
 import net.imglib2.algorithm.region.localneighborhood.RectangleNeighborhoodFactory;
-import net.imglib2.algorithm.region.localneighborhood.RectangleNeighborhoodRandomAccess;
 import net.imglib2.algorithm.region.localneighborhood.RectangleNeighborhoodSkipCenter;
 import net.imglib2.algorithm.region.localneighborhood.RectangleNeighborhoodSkipCenterUnsafe;
 import net.imglib2.algorithm.region.localneighborhood.RectangleNeighborhoodUnsafe;
 import net.imglib2.algorithm.region.localneighborhood.RectangleShape;
+import net.imglib2.algorithm.region.localneighborhood.RectangleShape.NeighborhoodsAccessible;
+import net.imglib2.algorithm.region.localneighborhood.RectangleShape.NeighborhoodsIterableInterval;
 import net.imglib2.algorithm.region.localneighborhood.Shape;
 import net.imglib2.util.Util;
 
@@ -74,7 +65,7 @@ import net.imglib2.util.Util;
  * <p>
  * This factory exists because {@link RectangleShape} which is based on the same
  * components, only allows for square neighborhoods.
- * 
+ *
  * @author Tobias Pietzsch <tobias.pietzsch@gmail.com>
  * @author Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
  */
@@ -91,7 +82,7 @@ public class CenteredRectangleShape implements Shape
 	 * The size of the neighborhood is specified by an <code>int[]</code> span
 	 * array, so that in every dimension <code>d</code>, the extent of the
 	 * neighborhood is given by <code>2 × span[d] + 1</code>.
-	 * 
+	 *
 	 * @param span
 	 *            the span of the neighborhood.
 	 * @param skipCenter
@@ -151,102 +142,5 @@ public class CenteredRectangleShape implements Shape
 			max[ d ] = span[ d ];
 		}
 		return new FinalInterval( min, max );
-	}
-
-	public static final class NeighborhoodsIterableInterval< T > extends AbstractInterval implements IterableInterval< Neighborhood< T > >
-	{
-		final RandomAccessibleInterval< T > source;
-
-		final Interval spanInterval;
-
-		final RectangleNeighborhoodFactory< T > factory;
-
-		final long size;
-
-		public NeighborhoodsIterableInterval( final RandomAccessibleInterval< T > source, final Interval span, final RectangleNeighborhoodFactory< T > factory )
-		{
-			super( source );
-			this.source = source;
-			this.spanInterval = span;
-			this.factory = factory;
-			long s = source.dimension( 0 );
-			for ( int d = 1; d < n; ++d )
-			{
-				s *= source.dimension( d );
-			}
-			size = s;
-		}
-
-		@Override
-		public Cursor< Neighborhood< T >> cursor()
-		{
-			return new RectangleNeighborhoodCursor< T >( source, spanInterval, factory );
-		}
-
-		@Override
-		public long size()
-		{
-			return size;
-		}
-
-		@Override
-		public Neighborhood< T > firstElement()
-		{
-			return cursor().next();
-		}
-
-		@Override
-		public Object iterationOrder()
-		{
-			return new FlatIterationOrder( this );
-		}
-
-		@Override
-		public Iterator< Neighborhood< T >> iterator()
-		{
-			return cursor();
-		}
-
-		@Override
-		public Cursor< Neighborhood< T >> localizingCursor()
-		{
-			return cursor();
-		}
-	}
-
-	public static final class NeighborhoodsAccessible< T > extends AbstractEuclideanSpace implements RandomAccessible< Neighborhood< T > >
-	{
-		final RandomAccessible< T > source;
-
-		final RectangleNeighborhoodFactory< T > factory;
-
-		final Interval spanInterval;
-
-		public NeighborhoodsAccessible( final RandomAccessible< T > source, final Interval span, final RectangleNeighborhoodFactory< T > factory )
-		{
-			super( source.numDimensions() );
-			this.source = source;
-			this.spanInterval = span;
-			this.factory = factory;
-		}
-
-		@Override
-		public RandomAccess< Neighborhood< T >> randomAccess()
-		{
-			return new RectangleNeighborhoodRandomAccess< T >( source, spanInterval, factory );
-		}
-
-		@Override
-		public RandomAccess< Neighborhood< T >> randomAccess( final Interval interval )
-		{
-			return randomAccess();
-		}
-
-		@Override
-		public int numDimensions()
-		{
-			return source.numDimensions();
-		}
-
 	}
 }
