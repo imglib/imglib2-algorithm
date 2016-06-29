7604b05e7c3799bd92bbe10c4353f534fcf37415
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2016-06-28 20:36:21 -0400

    formatted flood fill test according to ImgLib2 code style

--- src/test/java/net/imglib2/algorithm/fill/FloodFillTest.java
+++ src/test/java/net/imglib2/algorithm/fill/FloodFillTest.java
@@ -11,13 +11,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -34,6 +34,9 @@
 
 package net.imglib2.algorithm.fill;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import net.imglib2.Cursor;
 import net.imglib2.Point;
 import net.imglib2.algorithm.neighborhood.DiamondShape;
@@ -45,99 +48,95 @@ import net.imglib2.type.numeric.integer.LongType;
 import net.imglib2.util.Pair;
 import net.imglib2.view.ExtendedRandomAccessibleInterval;
 import net.imglib2.view.Views;
-import org.junit.Assert;
-import org.junit.Test;
 
 /**
  * @author Philipp Hanslovsky &lt;hanslovskyp@janelia.hhmi.org&gt;
  * @author Stephan Saalfeld &lt;saalfelds@janelia.hhmi.org&gt;
  */
-public class FloodFillTest {
+public class FloodFillTest
+{
+	private static final long START_LABEL = 1;
 
-    private static final long START_LABEL = 1;
-    private static final long FILL_LABEL = 2;
-    private static final int[] N_DIMS = { 1, 2, 3, 4 };
-    private static final int SIZE_OF_EACH_DIM = 60;
+	private static final long FILL_LABEL = 2;
 
+	private static final int[] N_DIMS = { 1, 2, 3, 4 };
 
-    private static < T extends IntegerType < T > > void runTest(
-            int nDim,
-            int sizeOfEachDim,
-            ImgFactory< T > imageFactory,
-            T t )
-    {
-        long[] dim = new long[nDim];
-        long[] c = new long[nDim];
-        long r = sizeOfEachDim / 4;
-        for ( int d = 0; d < nDim; ++d ) {
-            dim[d] = sizeOfEachDim;
-            c[d] = sizeOfEachDim / 3;
-        }
+	private static final int SIZE_OF_EACH_DIM = 60;
 
-        long divisionLine = r / 3;
+	private static < T extends IntegerType< T > > void runTest( final int nDim, final int sizeOfEachDim, final ImgFactory< T > imageFactory, final T t )
+	{
+		final long[] dim = new long[ nDim ];
+		final long[] c = new long[ nDim ];
+		final long r = sizeOfEachDim / 4;
+		for ( int d = 0; d < nDim; ++d )
+		{
+			dim[ d ] = sizeOfEachDim;
+			c[ d ] = sizeOfEachDim / 3;
+		}
 
-        Img<T> img = imageFactory.create(dim, t.copy() );
-        Img<T> refImg = imageFactory.create(dim, t.copy());
+		final long divisionLine = r / 3;
 
-        for (Cursor<T> i = img.cursor(), ref = refImg.cursor(); i.hasNext(); )
-        {
-            i.fwd();
-            ref.fwd();
-            long diffSum = 0;
-            for ( int d = 0; d < nDim; ++d )
-            {
-                long diff = i.getLongPosition( d ) - c[d];
-                diffSum += diff * diff;
+		final Img< T > img = imageFactory.create( dim, t.copy() );
+		final Img< T > refImg = imageFactory.create( dim, t.copy() );
 
-            }
+		for ( Cursor< T > i = img.cursor(), ref = refImg.cursor(); i.hasNext(); )
+		{
+			i.fwd();
+			ref.fwd();
+			long diffSum = 0;
+			for ( int d = 0; d < nDim; ++d )
+			{
+				final long diff = i.getLongPosition( d ) - c[ d ];
+				diffSum += diff * diff;
 
-            if ( ( diffSum < r * r ) ) {
-                if ((i.getLongPosition(0) - c[0] < divisionLine)) {
-                    i.get().setInteger(START_LABEL);
-                    ref.get().setInteger( FILL_LABEL );
-                } else if (i.getLongPosition(0) - c[0] > divisionLine) {
-                    i.get().setInteger( START_LABEL );
-                    ref.get().setInteger( START_LABEL );
-                }
-            }
+			}
 
-        }
+			if ( ( diffSum < r * r ) )
+			{
+				if ( ( i.getLongPosition( 0 ) - c[ 0 ] < divisionLine ) )
+				{
+					i.get().setInteger( START_LABEL );
+					ref.get().setInteger( FILL_LABEL );
+				}
+				else if ( i.getLongPosition( 0 ) - c[ 0 ] > divisionLine )
+				{
+					i.get().setInteger( START_LABEL );
+					ref.get().setInteger( START_LABEL );
+				}
+			}
 
-        T fillLabel = t.createVariable();
-        fillLabel.setInteger( FILL_LABEL );
+		}
 
-        ExtendedRandomAccessibleInterval<T, Img<T>> extendedImg = Views.extendValue(img, fillLabel);
+		final T fillLabel = t.createVariable();
+		fillLabel.setInteger( FILL_LABEL );
 
-        Filter<Pair<T, T>, Pair<T, T>> filter = new Filter< Pair< T, T >, Pair< T, T > >() {
-            @Override
-            public boolean accept(Pair< T, T > p1, Pair< T, T > p2) {
-                return ( p1.getB().getIntegerLong() != p2.getB().getIntegerLong() ) &&
-                        ( p1.getA().getIntegerLong() == p2.getA().getIntegerLong() );
-            }
-        };
+		final ExtendedRandomAccessibleInterval< T, Img< T > > extendedImg = Views.extendValue( img, fillLabel );
 
-        FloodFill.fill( extendedImg, extendedImg, new Point( c ), fillLabel, new DiamondShape( 1 ), filter );
+		final Filter< Pair< T, T >, Pair< T, T > > filter = new Filter< Pair< T, T >, Pair< T, T > >()
+		{
+			@Override
+			public boolean accept( final Pair< T, T > p1, final Pair< T, T > p2 )
+			{
+				return ( p1.getB().getIntegerLong() != p2.getB().getIntegerLong() ) && ( p1.getA().getIntegerLong() == p2.getA().getIntegerLong() );
+			}
+		};
 
-        for ( Cursor< T > imgCursor = img.cursor(), refCursor = refImg.cursor(); imgCursor.hasNext(); )
-        {
-            Assert.assertEquals( refCursor.next(), imgCursor.next() );
-        }
+		FloodFill.fill( extendedImg, extendedImg, new Point( c ), fillLabel, new DiamondShape( 1 ), filter );
 
+		for ( Cursor< T > imgCursor = img.cursor(), refCursor = refImg.cursor(); imgCursor.hasNext(); )
+		{
+			Assert.assertEquals( refCursor.next(), imgCursor.next() );
+		}
 
-    }
+	}
 
-    @Test
-    public void runTests()
-    {
-        for ( int nDim : N_DIMS )
-        {
-            runTest(
-                    nDim,
-                    SIZE_OF_EACH_DIM,
-                    new ArrayImgFactory<LongType>(),
-                    new LongType()
-            );
-        }
-    }
+	@Test
+	public void runTests()
+	{
+		for ( final int nDim : N_DIMS )
+		{
+			runTest( nDim, SIZE_OF_EACH_DIM, new ArrayImgFactory< LongType >(), new LongType() );
+		}
+	}
 
 }
