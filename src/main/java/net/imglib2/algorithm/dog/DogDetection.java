4b4b9c544631c0df93e972ef7ad0a0998b17fdb6
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2016-07-11 09:39:41 -0400

    added convenience method to start DoG with an RAinterval

--- src/main/java/net/imglib2/algorithm/dog/DogDetection.java
+++ src/main/java/net/imglib2/algorithm/dog/DogDetection.java
@@ -11,13 +11,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -78,10 +78,22 @@ public class DogDetection< T extends RealType< T > & NativeType< T > >
 		this( Views.extendMirrorSingle( input ), input, getcalib( input ), sigma1, sigma2, extremaType, minPeakValue, true );
 	}
 
+	public DogDetection(
+			final RandomAccessibleInterval< T > input,
+			final double[] calibration,
+			final double sigma1,
+			final double sigma2,
+			final ExtremaType extremaType,
+			final double minPeakValue,
+			final boolean normalizeMinPeakValue )
+	{
+		this( Views.extendMirrorSingle( input ), input, calibration, sigma1, sigma2, extremaType, minPeakValue, true );
+	}
+
 	/**
 	 * Sets up a {@link DogDetection} with the specified parameters (does not do
 	 * any computation yet).
-	 * 
+	 *
 	 * @param input
 	 *            the input image.
 	 * @param interval
