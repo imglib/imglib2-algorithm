9aecda0219955d10cd19552322d0c821954bdc9b
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-08-04 10:17:40 -0400

    Added parallel version for gradient.

--- src/main/java/net/imglib2/algorithm/gradient/PartialDerivative.java
+++ src/main/java/net/imglib2/algorithm/gradient/PartialDerivative.java
@@ -11,13 +11,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -34,12 +34,21 @@
 
 package net.imglib2.algorithm.gradient;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
 import net.imglib2.Cursor;
+import net.imglib2.FinalInterval;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.numeric.NumericType;
 import net.imglib2.util.Intervals;
+import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 
 public class PartialDerivative
@@ -47,7 +56,7 @@ public class PartialDerivative
 	// nice version...
 	/**
 	 * Compute the partial derivative of source in a particular dimension.
-	 * 
+	 *
 	 * @param source
 	 *            source image, has to provide valid data in the interval of the
 	 *            gradient image plus a one pixel border in dimension.
@@ -69,10 +78,68 @@ public class PartialDerivative
 		}
 	}
 
+	// parallel version...
+	public static < T extends NumericType< T > > void gradientCentralDifferenceParallel(
+			final RandomAccessible< T > source,
+			final RandomAccessibleInterval< T > gradient,
+			final int dimension,
+			final int nThreads,
+			final ExecutorService es ) throws InterruptedException, ExecutionException
+	{
+		final int nDim = source.numDimensions();
+		if ( nDim < 2 )
+		{
+			gradientCentralDifference2( source, gradient, dimension );
+			return;
+		}
+
+		long dimensionMax = Long.MIN_VALUE;
+		int dimensionArgMax = -1;
+
+		for ( int d = 0; d < nDim; ++d )
+		{
+			final long size = gradient.dimension( d );
+			if ( d != dimension && size > dimensionMax )
+			{
+				dimensionMax = size;
+				dimensionArgMax = d;
+			}
+		}
+
+		final long stepSize = Math.max( dimensionMax / nThreads, 1 );
+		final long stepSizeMinusOne = stepSize - 1;
+		final long min = gradient.min( dimensionArgMax );
+		final long max = gradient.max( dimensionArgMax );
+
+		final ArrayList< Callable< Void > > tasks = new ArrayList<>();
+		for ( long currentMin = min, minZeroBase = 0; minZeroBase < dimensionMax; currentMin += stepSize, minZeroBase += stepSize )
+		{
+			final long currentMax = Math.min( currentMin + stepSizeMinusOne, max );
+			final long[] mins = new long[ nDim ];
+			final long[] maxs = new long[ nDim ];
+			gradient.min( mins );
+			gradient.max( maxs );
+			mins[ dimensionArgMax ] = currentMin;
+			maxs[ dimensionArgMax ] = currentMax;
+			final IntervalView< T > currentInterval = Views.interval( gradient, new FinalInterval( mins, maxs ) );
+			tasks.add( () -> {
+				gradientCentralDifference( source, currentInterval, dimension );
+				return null;
+			} );
+		}
+
+		final List< Future< Void > > futures = es.invokeAll( tasks );
+
+		for ( final Future< Void > f : futures )
+		{
+			f.get();
+		}
+	}
+
 	// fast version
 	/**
 	 * Compute the partial derivative of source in a particular dimension.
-	 * 
+	 *
 	 * @param source
 	 *            source image, has to provide valid data in the interval of the
 	 *            gradient image plus a one pixel border in dimension.
@@ -91,7 +158,9 @@ public class PartialDerivative
 		gradient.max( max );
 		final long[] shiftback = new long[ n ];
 		for ( int d = 0; d < n; ++d )
+		{
 			shiftback[ d ] = min[ d ] - max[ d ];
+		}
 
 		final RandomAccess< T > result = gradient.randomAccess();
 		final RandomAccess< T > back = source.randomAccess( Intervals.translate( gradient, 1, dimension ) );
@@ -117,20 +186,23 @@ public class PartialDerivative
 			// iterations
 			if ( result.getLongPosition( 0 ) == max0 )
 			{
-				if ( n == 1 )
+				if ( n == 1 ) {
 					return;
+				}
 				result.move( shiftback[ 0 ], 0 );
 				back.move( shiftback[ 0 ], 0 );
 				front.move( shiftback[ 0 ], 0 );
 				// now check the remaining dimensions
 				for ( int d = 1; d < n; ++d )
+				{
 					if ( result.getLongPosition( d ) == max[ d ] )
 					{
 						result.move( shiftback[ d ], d );
 						back.move( shiftback[ d ], d );
 						front.move( shiftback[ d ], d );
-						if ( d == n - 1 )
+						if ( d == n - 1 ) {
 							return;
+						}
 					}
 					else
 					{
@@ -139,6 +211,7 @@ public class PartialDerivative
 						front.fwd( d );
 						break;
 					}
+				}
 			}
 			else
 			{
