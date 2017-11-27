68478fb06e75338855d5d6d26817763f556ab429
kapoorlab <kapoorconsciousness@gmail.com>
2017-11-27 16:08:29 +0100

    Removing Spurious ellipses

--- src/main/java/net/imglib2/algorithm/ransac/RansacModels/RansacEllipsoid.java
+++ src/main/java/net/imglib2/algorithm/ransac/RansacModels/RansacEllipsoid.java
@@ -1,11 +1,13 @@
 package net.imglib2.algorithm.ransac.RansacModels;
 
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 import ij.gui.EllipseRoi;
+import ij.gui.Roi;
 import net.imglib2.RealLocalizable;
 import net.imglib2.RealPoint;
 import net.imglib2.algorithm.ransac.RansacModels.DistPointHyperEllipsoid.Result;
@@ -18,6 +20,7 @@ import java.util.List;
 
 import net.imagej.DrawingTool;
 import net.imglib2.Cursor;
+import net.imglib2.KDTree;
 import net.imglib2.RandomAccess;
 import net.imglib2.RealLocalizable;
 import net.imglib2.RealPoint;
@@ -27,6 +30,7 @@ import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.basictypeaccess.array.FloatArray;
+import net.imglib2.neighborsearch.NearestNeighborSearchOnKDTree;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Pair;
 import net.imglib2.util.Util;
@@ -36,30 +40,58 @@ public class RansacEllipsoid {
 
 	
 	
-	public static List<Pair<int[], int[]>> GetEllipsepoints(Ellipsoid ellipse){
+	public static List<double[]> GetEllipsepoints(Ellipsoid ellipse){
 		
-		List<Pair<int[], int[]>> pointlist = new ArrayList<Pair<int[], int[]>>();
+		List<double[]> pointlist = new ArrayList<double[]>();
 		
 		EllipseRoi roi = DisplayEllipse.create2DEllipse(ellipse.getCenter(), new double[] {ellipse.getCovariance()[0][0], ellipse.getCovariance()[0][1], ellipse.getCovariance()[1][1] });
 		
-		
+    int N = roi.getNCoordinates();	
+    Rectangle bounds = roi.getBounds();
      int[] xCord = roi.getXCoordinates();
 	 int[] yCord = roi.getYCoordinates();	
 	 
-	 pointlist.add(new ValuePair<int[] , int[]> (xCord, yCord));
+	
 	 
-	 for (int index = 0; index < pointlist.size(); ++index)
-	 System.out.println(pointlist.get(index).getA());
-		
+	 for (int index = 0; index < N; ++index) {
+	
+	 
+	 pointlist.add(new double[] {bounds.x + xCord[index], bounds.y + yCord[index]});
+	 
+	 }
 		return pointlist;
 	}
 	
 	
-	
+	public static <T extends Comparable<T>>  double GetnearestPoint(List<Pair<RealLocalizable, T>> targetlist, double[] sourcepoint) {
+		
+		
+		ArrayList<RealLocalizable> pointlist = new ArrayList<RealLocalizable>();
+		
+		for (Pair<RealLocalizable, T> point: targetlist) {
+			
+			pointlist.add(point.getA());
+			
+		}
+		
+		
+		final KDTree<RealLocalizable> tree = new KDTree<RealLocalizable>(pointlist, pointlist);
+		
+		
+		NearestNeighborSearchOnKDTree<RealLocalizable> Search = new NearestNeighborSearchOnKDTree<>(tree);
+		final RealPoint rpos = new RealPoint(sourcepoint);
+		
+		Search.search(rpos);
+		double distance = Search.getSquareDistance();
+		
+		return distance;
+		
+		
+	}
 	
 	public static <T extends Comparable<T>>  ArrayList<Pair<Pair<Ellipsoid, GeneralEllipsoid>, List<Pair<RealLocalizable, T>>>> Allsamples(
 			final List<Pair<RealLocalizable, T>> points, final double outsideCutoffDistance,
-			final double insideCutoffDistance, int minsize, final NumericalSolvers numsol) {
+			final double insideCutoffDistance, double minpercent, final NumericalSolvers numsol) {
 
 		boolean fitted;
 
@@ -78,10 +110,22 @@ public class RansacEllipsoid {
 				final Pair<Pair<Ellipsoid, GeneralEllipsoid>, List<Pair<RealLocalizable, T>>> f = sample(remainingPoints,
 						remainingPoints.size(), outsideCutoffDistance, insideCutoffDistance, numsol);
 
+				if (f!=null) {
+				List<double[]> pointlist = GetEllipsepoints(f.getA().getA());
+				double size = pointlist.size();
+				double count = 0;
+				for (int index = 0; index < pointlist.size(); ++index) {
+					
+					double distance = GetnearestPoint(remainingPoints, pointlist.get(index));
+					
+					if (distance > outsideCutoffDistance || distance > insideCutoffDistance)
+						count++;
+				}
 				
-				GetEllipsepoints(f.getA().getA());
 				
-				if (f != null && f.getB().size() > minsize) {
+				
+				
+				if ( f.getB().size() > 9) {
 
 					fitted = true;
 
@@ -91,9 +135,24 @@ public class RansacEllipsoid {
 					for (final Pair<RealLocalizable, T> p : f.getB())
 						inlierPoints.add(p);
 					remainingPoints.removeAll(inlierPoints);
+					
 
 				}
+				double percent = (size - count) / size;
+				
+				if (percent < minpercent) {
+					
+					System.out.println("Wrong Ellipse detected, removing" + count + " " + size);
 
+					segments.remove(f);
+					final List<Pair<RealLocalizable, T>> inlierPoints = new ArrayList<Pair<RealLocalizable, T>>();
+					for (final Pair<RealLocalizable, T> p : f.getB())
+						inlierPoints.add(p);
+					remainingPoints.addAll(inlierPoints);
+					
+					
+				}
+				}
 			} else {
 
 				fitted = true;
