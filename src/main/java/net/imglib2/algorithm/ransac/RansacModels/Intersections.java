5ba323e73a702de1fd8255226fc21f7c081f09fb
kapoorlab <kapoorconsciousness@gmail.com>
2017-11-28 17:49:03 +0100

    COmmenting out error prone listing1 and 2 of intersecting solutions

--- src/main/java/net/imglib2/algorithm/ransac/RansacModels/Intersections.java
+++ src/main/java/net/imglib2/algorithm/ransac/RansacModels/Intersections.java
@@ -97,7 +97,9 @@ public class Intersections {
 		double e2xbar = e0 + e1 * xbar + e2 * xbar * xbar;
 		ArrayList<double[]> intersection = new ArrayList<>();
 		if (c2 > 0 && c1 * c1 > 4 * c0 * c2) {
-		if (Math.abs(d4) >= 1.0E-3 && Math.abs(e2xbar) >= 1.0E-3) {
+			
+			/*
+		if (Math.abs(d4) >= 1.0E-1 && Math.abs(e2xbar) >= 1.0E-3) {
 
 			// Listing 1 and 2 David Eberly, intersection of ellipses text
 
@@ -170,9 +172,9 @@ public class Intersections {
 
 
 		}
-
-		else if (Math.abs(d4) <= 1.0E-3 && d2 != 0 && e2 != 0) {
-
+*/
+		//if (Math.abs(d4) <= 1.0E-1 && d2 != 0 && e2 != 0) {
+			if (d2 != 0 && e2 != 0) {
 			// Listing 3 David Eberly, interesection of ellipses text
 			System.out.println("Intersection: Listing 3 Solution");
 			double f0 = c0 * d2 * d2 + e0 * e0;
@@ -193,8 +195,9 @@ public class Intersections {
 
 		}
 
-		else if (Math.abs(d4) <= 1.0E-3 && d2 != 0 && Math.abs(e2) <= 1.0E-3) {
-
+	//	else if (Math.abs(d4) <= 1.0E-3 && d2 != 0 && Math.abs(e2) <= 1.0E-3) {
+			else if (d2 != 0 && Math.abs(e2) <= 1.0E-3) {
+			
 			// Listing 4 David Eberly, interesection of ellipses text
 			System.out.println("Intersection: Listing 4 Solution");
 			double f0 = c0 * d2 * d2 + e0 * e0;
@@ -215,7 +218,8 @@ public class Intersections {
 
 		}
 
-		else 	if (Math.abs(d4) <= 1.0E-3 && Math.abs(d2) <= 1.0E-3 && Math.abs(e2) <= 1.0E-3) {
+	//	else 	if (Math.abs(d4) <= 1.0E-3 && Math.abs(d2) <= 1.0E-3 && Math.abs(e2) <= 1.0E-3) {
+				else 	if (Math.abs(d2) <= 1.0E-3 && Math.abs(e2) <= 1.0E-3) {
 
 			// Listing 5 David Eberly, intersection of ellipses text
 			System.out.println("Intersection: Listing 5 Solution");
@@ -248,7 +252,10 @@ public class Intersections {
 
 		}
 
-		else if (Math.abs(d4) <= 1.0E-3 && Math.abs(d2) <= 1.0E-3 && e2 != 0) {
+	//	else if (Math.abs(d4) <= 1.0E-3 && Math.abs(d2) <= 1.0E-3 && e2 != 0) {
+				else if (Math.abs(d2) <= 1.0E-3 && e2 != 0) {
+
+			
 			// Listing 6 David Eberly intersection of ellipses
 			System.out.println("Intersection: Listing 6 Solution");
 			
