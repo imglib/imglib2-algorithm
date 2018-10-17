ac0dbee733b3b1a35015f5129dbca80dd3ea6dc0
tpietzsch <tobias.pietzsch@gmail.com>
2018-10-17 13:09:03 +0200

    Remove unused method

--- src/main/java/net/imglib2/algorithm/convolution/LineConvolverFactory.java
+++ src/main/java/net/imglib2/algorithm/convolution/LineConvolverFactory.java
@@ -45,29 +45,4 @@ public interface LineConvolverFactory< T >
 	 * (Input line length should be getBorderBefore() + lineLength + getBorderAfter())
 	 */
 	Runnable getConvolver( RandomAccess< ? extends T > in, RandomAccess< ? extends T > out, int d, long lineLength );
-
-	static < S, T > LineConvolverFactory< T > wrapGauss3Convolver( double[] halfkernel, net.imglib2.algorithm.gauss3.ConvolverFactory< S, T > old )
-	{
-		return new LineConvolverFactory< T >()
-		{
-			@Override public long getBorderBefore()
-			{
-				return halfkernel.length - 1;
-			}
-
-			@Override public long getBorderAfter()
-			{
-				return halfkernel.length - 1;
-			}
-
-			@Override public Runnable getConvolver( RandomAccess< ? extends T > in, RandomAccess< ? extends T > out, int d, long lineLength )
-			{
-				@SuppressWarnings( "unchecked" )
-				RandomAccess< S > in1 = ( RandomAccess< S > ) in;
-				@SuppressWarnings( "unchecked" )
-				RandomAccess< T > out1 = ( RandomAccess< T > ) out;
-				return old.create( halfkernel, in1, out1, d, lineLength );
-			}
-		};
-	}
 }
