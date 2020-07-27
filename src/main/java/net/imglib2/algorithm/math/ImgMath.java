5c82415c4f47c3ada182225712fdd22e3759a9ad
Albert Cardona <sapristi@gmail.com>
2020-07-27 15:37:40 +0100

    ImgMath: add pow as an operation, following the addition of the Pow interface to NumericType.

--- src/main/java/net/imglib2/algorithm/math/ImgMath.java
+++ src/main/java/net/imglib2/algorithm/math/ImgMath.java
@@ -149,6 +149,26 @@ public class ImgMath
 		return new Div( obs );
 	}
 	
+	static public final Pow pow( final Object o1, final Object o2 )
+	{
+		return new Pow( o1, o2 );
+	}
+	
+	static public final Pow power( final Object... obs )
+	{
+		return new Pow( obs );
+	}
+	
+	static public final Pow power( final Object o1, final Object o2 )
+	{
+		return new Pow( o1, o2 );
+	}
+	
+	static public final Pow pow( final Object... obs )
+	{
+		return new Pow( obs );
+	}
+
 	static public final Max max( final Object o1, final Object o2 )
 	{
 		return new Max( o1, o2 );
---
+++ src/main/java/net/imglib2/algorithm/math/execution/Power.java
@@ -0,0 +1,55 @@
+package net.imglib2.algorithm.math.execution;
+
+import java.util.Arrays;
+import java.util.List;
+
+import net.imglib2.Localizable;
+import net.imglib2.algorithm.math.abstractions.OFunction;
+import net.imglib2.type.numeric.RealType;
+
+public class Power< O extends RealType< O > > implements OFunction< O >
+{
+	private final OFunction< O > a, b;
+	private final O scrap;
+	
+	public Power( final O scrap, final OFunction< O > a, final OFunction< O > b )
+	{
+		this.scrap = scrap;
+		this.a = a;
+		this.b = b;
+	}
+	
+	@Override
+	public final O eval()
+	{
+		this.scrap.set( this.a.eval() );
+		this.scrap.pow( this.b.eval() );
+		return this.scrap;
+	}
+
+	@Override
+	public final O eval( final Localizable loc )
+	{
+		this.scrap.set( this.a.eval( loc ) );
+		this.scrap.pow( this.b.eval( loc ) );
+		return this.scrap;
+	}
+	
+	@Override
+	public List< OFunction< O > > children()
+	{
+		return Arrays.asList( this.a, this.b );
+	}
+	
+	@Override
+	public final double evalDouble()
+	{
+		return Math.pow( this.a.evalDouble(), this.b.evalDouble() );
+	}
+	
+	@Override
+	public final double evalDouble( final Localizable loc )
+	{
+		return Math.pow( this.a.evalDouble( loc ), this.b.evalDouble( loc ) );
+	}
+}
