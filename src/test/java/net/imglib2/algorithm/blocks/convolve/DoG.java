3b54dc0122aec7dd5a36d35d13c348bb647501a3
tpietzsch <tobias.pietzsch@gmail.com>
2024-07-22 16:48:01 +0200

    WIP DoG operator and operator factory

---
+++ src/test/java/net/imglib2/algorithm/blocks/convolve/DoG.java
@@ -0,0 +1,114 @@
+package net.imglib2.algorithm.blocks.convolve;
+
+import static net.imglib2.algorithm.blocks.convolve.Convolve.gaussKernels;
+import static net.imglib2.type.PrimitiveType.FLOAT;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+import net.imglib2.algorithm.blocks.BlockProcessor;
+import net.imglib2.algorithm.blocks.BlockSupplier;
+import net.imglib2.algorithm.blocks.DefaultUnaryBlockOperator;
+import net.imglib2.algorithm.blocks.UnaryBlockOperator;
+import net.imglib2.algorithm.blocks.convert.ClampType;
+import net.imglib2.algorithm.convolution.kernel.Kernel1D;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.PrimitiveType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.type.numeric.real.FloatType;
+
+/**
+ * Difference of Gaussian.
+ * <p>
+ * TODO: document supported types etc similar to Convolve
+ */
+public class DoG
+{
+	public static < T extends NativeType< T > >
+	Function< BlockSupplier< T >, UnaryBlockOperator< T, T > > DoG( final Convolve.ComputationType computationType, final double k, final double... sigmaSmaller )
+	{
+		final double[] sigmaLarger = new double[ sigmaSmaller.length ];
+		Arrays.setAll( sigmaLarger, i -> sigmaSmaller[ i ] * k );
+		return DoG( computationType, sigmaSmaller, sigmaLarger );
+	}
+
+	public static < T extends NativeType< T > >
+	Function< BlockSupplier< T >, UnaryBlockOperator< T, T > > DoG( final Convolve.ComputationType computationType, final double[] sigmaSmaller, final double[] sigmaLarger )
+	{
+		return s -> {
+			final T type = s.getType();
+			final int n = s.numDimensions();
+			return createOperator( type, computationType, gaussKernels( n, sigmaSmaller ), gaussKernels( n, sigmaLarger ) );
+		};
+	}
+
+	/**
+	 * TODO: javadoc
+	 * <p>
+	 * Create a {@code UnaryBlockOperator} to compute DoG with the given sigmas...
+	 * TODO: see Convolve.createOperator javadoc
+	 *
+	 * @param type
+	 * 		instance of the input type
+	 * @param computationType
+	 * 		specifies in which precision intermediate values should be
+	 * 		computed. For {@code AUTO}, the type that can represent the
+	 * 		input/output type without loss of precision is picked. That is,
+	 * 		        {@code FLOAT} for u8, i8, u16, i16, i32, f32, and otherwise {@code
+     * 		DOUBLE} for u32, i64, f64.
+     * @param kernelsSmaller
+     * 		kernel to apply in each dimension (smaller Gaussian)
+     * @param kernelsLarger
+     * 		kernel to apply in each dimension (larger Gaussian)
+     * @param <T>
+     * 		the input/output type
+     *
+     * @return {@code UnaryBlockOperator} to compute DoG for blocks of type {@code T}
+     */
+	public static < T extends NativeType< T > >
+	UnaryBlockOperator< T, T > createOperator( final T type, final Convolve.ComputationType computationType, final Kernel1D[] kernelsSmaller, final Kernel1D[] kernelsLarger )
+	{
+		final boolean processAsFloat;
+		switch ( computationType )
+		{
+		case FLOAT:
+			processAsFloat = true;
+			break;
+		case DOUBLE:
+			processAsFloat = false;
+			break;
+		default:
+		case AUTO:
+			final PrimitiveType pt = type.getNativeTypeFactory().getPrimitiveType();
+			processAsFloat = pt.equals( FLOAT ) || pt.getByteCount() < FLOAT.getByteCount();
+			break;
+		}
+		final UnaryBlockOperator< ?, ? > op = processAsFloat
+				? dogFloat( kernelsSmaller, kernelsLarger )
+				: dogDouble( kernelsSmaller, kernelsLarger );
+		return op.adaptSourceType( type, ClampType.NONE ).adaptTargetType( type, ClampType.CLAMP );
+	}
+
+	private static UnaryBlockOperator< FloatType, FloatType > dogFloat( final Kernel1D[] kernelsSmaller, final Kernel1D[] kernelsLarger )
+	{
+		final FloatType type = new FloatType();
+		final int n = kernelsLarger.length;
+		BlockProcessor p0 = new ConvolveProcessors.ConvolveFloat( kernelsSmaller );
+		BlockProcessor p1 = new ConvolveProcessors.ConvolveFloat( kernelsLarger );
+		// if we put it in a package other than convolve, use the following instead:
+//			BlockProcessor p0 = Convolve.createOperator( type, ComputationType.AUTO, kernelsSmaller ).blockProcessor();
+//			BlockProcessor p1 = Convolve.createOperator( type, ComputationType.AUTO, kernelsLarger ).blockProcessor();
+		BlockProcessor proc = new DoGProcessor<>( type, p0, p1 );
+		return new DefaultUnaryBlockOperator<>( type, type, n, n, proc );
+	}
+
+	private static UnaryBlockOperator< DoubleType, DoubleType > dogDouble( final Kernel1D[] kernelsSmaller, final Kernel1D[] kernelsLarger )
+	{
+		final DoubleType type = new DoubleType();
+		final int n = kernelsLarger.length;
+		BlockProcessor p0 = new ConvolveProcessors.ConvolveDouble( kernelsSmaller );
+		BlockProcessor p1 = new ConvolveProcessors.ConvolveDouble( kernelsLarger );
+		BlockProcessor proc = new DoGProcessor<>( type, p0, p1 );
+		return new DefaultUnaryBlockOperator<>( type, type, n, n, proc );
+	}
+}
--- src/test/java/net/imglib2/algorithm/blocks/convolve/DogBdvPlayground.java
+++ src/test/java/net/imglib2/algorithm/blocks/convolve/DogBdvPlayground.java
@@ -33,8 +33,6 @@
  */
 package net.imglib2.algorithm.blocks.convolve;
 
-import java.util.function.Function;
-
 import bdv.cache.SharedQueue;
 import bdv.util.Bdv;
 import bdv.util.BdvFunctions;
@@ -43,19 +41,15 @@ import bdv.util.volatiles.VolatileViews;
 import bdv.viewer.DisplayMode;
 import ij.IJ;
 import ij.ImagePlus;
-import net.imglib2.FinalInterval;
 import net.imglib2.algorithm.blocks.BlockAlgoUtils;
-import net.imglib2.algorithm.blocks.BlockProcessor;
 import net.imglib2.algorithm.blocks.BlockSupplier;
-import net.imglib2.algorithm.blocks.DefaultUnaryBlockOperator;
-import net.imglib2.algorithm.blocks.UnaryBlockOperator;
 import net.imglib2.algorithm.blocks.convert.Convert;
+import net.imglib2.algorithm.blocks.convolve.Convolve.ComputationType;
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.type.numeric.real.FloatType;
-import net.imglib2.util.Intervals;
 import net.imglib2.view.Views;
 
 public class DogBdvPlayground
@@ -70,53 +64,9 @@ public class DogBdvPlayground
 
 
 		final int sensitivity = 4;
-		final double sigmaSmaller = 2;
-		final double sigmaLarger = computeSigma2( sigmaSmaller, sensitivity );
+		final double sigmaSmaller = 8;
+		final double k = k( sensitivity );
 
-		System.out.println( "sigmaSmaller = " + sigmaSmaller );
-		System.out.println( "sigmaLarger = " + sigmaLarger );
-
-
-		final BlockSupplier< FloatType > blocks = BlockSupplier.of( Views.extendMirrorDouble( img ) ).andThen( Convert.convert( new FloatType() ) );
-
-		final Function< BlockSupplier< FloatType >, UnaryBlockOperator< FloatType, FloatType > >
-				g1 = Convolve.gauss( sigmaSmaller );
-		final Function< BlockSupplier< FloatType >, UnaryBlockOperator< FloatType, FloatType > >
-				g2 = Convolve.gauss( sigmaLarger );
-
-		final UnaryBlockOperator< FloatType, FloatType > gOp1 = g1.apply( blocks );
-		final UnaryBlockOperator< FloatType, FloatType > gOp2 = g2.apply( blocks );
-
-		final BlockProcessor< ?, ? > gProc1 = gOp1.blockProcessor();
-		final BlockProcessor< ?, ? > gProc2 = gOp2.blockProcessor();
-
-		final FinalInterval targetInterval = Intervals.createMinSize( 0, 0, 0, 10, 10, 10 );
-		System.out.println( "targetInterval = " + Intervals.toString( targetInterval ) );
-
-		gProc1.setTargetInterval( targetInterval );
-		System.out.println( "gProc1.getSourceInterval() = " + Intervals.toString( gProc1.getSourceInterval() ) );
-		;
-
-		gProc2.setTargetInterval( targetInterval );
-		System.out.println( "gProc2.getSourceInterval() = " + Intervals.toString( gProc2.getSourceInterval() ) );
-		;
-
-		final DoGProcessor jp = new DoGProcessor(
-				gOp1.getSourceType(), gOp1.getTargetType(), gProc1, gProc2 );
-		jp.setTargetInterval( targetInterval );
-		System.out.println( "jp.getSourceInterval() = " + Intervals.toString( jp.getSourceInterval() ) );
-
-
-		final FloatType type = new FloatType();
-		final UnaryBlockOperator< FloatType, FloatType > joined = new DefaultUnaryBlockOperator<>( type, type, 3, 3, jp );
-
-
-		final long[] dimensions = img.dimensionsAsLongArray();
-		final int[] cellDimensions = { 64, 64, 64 };
-		final Img< FloatType > convolved = BlockAlgoUtils.cellImg(
-				blocks.andThen( joined ),
-				dimensions,
-				cellDimensions );
 
 		final BdvSource bdv = BdvFunctions.show(
 				img,
@@ -126,8 +76,21 @@ public class DogBdvPlayground
 		bdv.setDisplayRange( 0, 255 );
 		bdv.getBdvHandle().getViewerPanel().setDisplayMode( DisplayMode.SINGLE );
 
+
+		final BlockSupplier< FloatType > blocks = BlockSupplier
+				.of( Views.extendMirrorDouble( img ) )
+				.andThen( Convert.convert( new FloatType() ) )
+				.andThen( DoG.DoG( ComputationType.AUTO, k, sigmaSmaller ) );
+
+		final long[] dimensions = img.dimensionsAsLongArray();
+		final int[] cellDimensions = { 64, 64, 64 };
+		final Img< FloatType > dog = BlockAlgoUtils.cellImg(
+				blocks,
+				dimensions,
+				cellDimensions );
+
 		final BdvSource out = BdvFunctions.show(
-				VolatileViews.wrapAsVolatile( convolved, new SharedQueue( 8, 1 ) ),
+				VolatileViews.wrapAsVolatile( dog, new SharedQueue( 8, 1 ) ),
 				"DoG",
 				Bdv.options().addTo( bdv )
 		);
@@ -139,8 +102,12 @@ public class DogBdvPlayground
 
 	private static double computeSigma2( final double sigma1, final int stepsPerOctave )
 	{
-		final double k = Math.pow( 2f, 1f / stepsPerOctave );
-		return sigma1 * k;
+		return sigma1 * k( stepsPerOctave );
+	}
+
+	private static double k( final int stepsPerOctave )
+	{
+		return Math.pow( 2f, 1f / stepsPerOctave );
 	}
 
 }
