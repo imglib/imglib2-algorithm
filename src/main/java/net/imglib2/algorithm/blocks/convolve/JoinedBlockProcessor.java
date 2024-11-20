cdee15d55f28e3b86783790093e534ee8e41f231
tpietzsch <tobias.pietzsch@gmail.com>
2024-11-20 20:26:24 +0100

    fix compile errors

--- src/main/java/net/imglib2/algorithm/blocks/convolve/JoinedBlockProcessor.java
+++ src/main/java/net/imglib2/algorithm/blocks/convolve/JoinedBlockProcessor.java
@@ -8,6 +8,7 @@ import net.imglib2.EuclideanSpace;
 import net.imglib2.Interval;
 import net.imglib2.algorithm.blocks.AbstractDimensionlessBlockProcessor;
 import net.imglib2.algorithm.blocks.BlockProcessor;
+import net.imglib2.blocks.BlockInterval;
 import net.imglib2.blocks.SubArrayCopy;
 import net.imglib2.blocks.TempArray;
 import net.imglib2.type.NativeType;
@@ -27,8 +28,7 @@ abstract class JoinedBlockProcessor< I, O0, O1, P > extends AbstractDimensionles
 
 	private SubArrayExtractor< I > subArray;
 
-	long[] destPos;
-	int[] destSize;
+	BlockInterval destInterval;
 	int destLength;
 
 	public < S extends NativeType< S >, T0 extends NativeType< T0 >, T1 extends NativeType< T1 > > JoinedBlockProcessor(
@@ -62,37 +62,35 @@ abstract class JoinedBlockProcessor< I, O0, O1, P > extends AbstractDimensionles
 	@Override
 	public void setTargetInterval( final Interval interval )
 	{
+		final int m = interval.numDimensions();
+		if ( destInterval == null || destInterval.numDimensions() != m )
+			destInterval = new BlockInterval( m );
+		destInterval.setFrom( interval );
+		destLength = safeInt( Intervals.numElements( destInterval ) );
+
 		p0.setTargetInterval( interval );
 		p1.setTargetInterval( interval );
+		final Interval sourceInterval0 = p0.getSourceInterval();
+		final Interval sourceInterval1 = p1.getSourceInterval();
 
-		final long[] sourcePos0 = p0.getSourcePos();
-		final long[] sourcePos1 = p1.getSourcePos();
-		final int[] sourceSize0 = p0.getSourceSize();
-		final int[] sourceSize1 = p1.getSourceSize();
-
-		final int m = interval.numDimensions();
-		if ( destPos == null || destPos.length != m )
+		final int n = sourceInterval0.numDimensions();
+		assert n == sourceInterval1.numDimensions();
+		if( updateNumSourceDimensions( n ) )
 		{
-			destPos = new long[ m ];
-			destSize = new int[ m ];
+			subArray = new SubArrayExtractor<>( n, copy );
 		}
-		interval.min( destPos );
-		Arrays.setAll( destSize, d -> Util.safeInt( interval.dimension( d ) ) );
-		destLength = safeInt( Intervals.numElements( destSize ) );
 
 		// Take the union of the source intervals of the merged processors.
 		// Use a separate sourceBuffer TempArray (because the union could be
 		// bigger than both of the processors' source intervals).
-		final int n = sourcePos0.length;
-		assert n == sourcePos1.length;
-		if( updateNumSourceDimsensions( n ) )
-		{
-			subArray = new SubArrayExtractor<>( n, copy );
-		}
-		Arrays.setAll( sourcePos, d -> Math.min( sourcePos0[ d ], sourcePos1[ d ] ) );
-		Arrays.setAll( sourceSize, d -> Util.safeInt( Math.max(
-				sourcePos0[ d ] + sourceSize0[ d ],
-				sourcePos1[ d ] + sourceSize1[ d ] ) - sourcePos[ d ] ) );
+		final long[] sourcePos = getSourceInterval().min();
+		final int[] sourceSize = getSourceInterval().size();
+		Arrays.setAll( sourcePos, d -> Math.min( sourceInterval0.min( d ), sourceInterval1.min( d ) ) );
+		Arrays.setAll( sourceSize, d -> Util.safeInt(
+				Math.max(
+						sourceInterval0.min( d ) + sourceInterval0.dimension( d ),
+						sourceInterval1.min( d ) + sourceInterval1.dimension( d )
+				) - sourcePos[ d ] ) );
 	}
 
 	@Override
@@ -102,11 +100,11 @@ abstract class JoinedBlockProcessor< I, O0, O1, P > extends AbstractDimensionles
 		// processor (or both), use src directly as input. Otherwise, use
 		// SubArrayCopy.
 
-		final I src0 = subArray.extract( src, sourcePos, sourceSize, p0 );
+		final I src0 = subArray.extract( src, getSourceInterval(), p0 );
 		final O0 dest0 = tempArrayDest0.get( destLength );
 		p0.compute( src0, dest0 );
 
-		final I src1 = subArray.extract( src, sourcePos, sourceSize, p1 );
+		final I src1 = subArray.extract( src, getSourceInterval(), p1 );
 		final O1 dest1 = tempArrayDest1.get( destLength );
 		p1.compute( src1, dest1 );
 
@@ -131,24 +129,24 @@ abstract class JoinedBlockProcessor< I, O0, O1, P > extends AbstractDimensionles
 		}
 
 		/**
-		 * If the given source interval (specified by {@code sourcePos}, {@code
-		 * sourceSize}) matches the source interval of {@code p0}, just return
-		 * {@code src}. Otherwise, {@link BlockProcessor#getSourceBuffer() get a
-		 * buffer} from {@code p0}, copy the appropriate region of {@code src}
-		 * into it, and return it.
+		 * If the given {@code interval} matches the source interval of {@code
+		 * p0}, just return {@code src}. Otherwise, {@link
+		 * BlockProcessor#getSourceBuffer() get a buffer} from {@code p0}, copy
+		 * the appropriate region of {@code src} into it, and return it.
 		 */
-		I extract( final I src, final long[] sourcePos, final int[] sourceSize, final BlockProcessor< I, ? > p0 )
+		I extract( final I src, final BlockInterval interval, final BlockProcessor< I, ? > p0 )
 		{
-			if ( Arrays.equals( sourcePos, p0.getSourcePos() ) && Arrays.equals( sourceSize, p0.getSourceSize() ) )
+			if ( Intervals.equals( interval, p0.getSourceInterval() ) )
 			{
 				return src;
 			}
 			else
 			{
-				final int[] destSize = p0.getSourceSize();
+				final int[] sourceSize = interval.size();
+				final int[] destSize = BlockInterval.asBlockInterval( p0.getSourceInterval() ).size();
 				IntervalIndexer.createAllocationSteps( sourceSize, sourceStrides );
 				IntervalIndexer.createAllocationSteps( destSize, destStrides );
-				Arrays.setAll( relSourcePos, d -> ( int ) ( p0.getSourcePos()[ d ] - sourcePos[ d ] ) );
+				Arrays.setAll( relSourcePos, d -> ( int ) ( p0.getSourceInterval().min( d ) - interval.min( d ) ) );
 				final int oSrc = IntervalIndexer.positionToIndex( relSourcePos, sourceSize );
 				final int oDest = 0;
 
