bf8ccb8942dc229cb40c52da33ca96a8393a1b34
tpietzsch <tobias.pietzsch@gmail.com>
2025-12-18 21:54:39 +0100

    WIP: Lookup2DProcessor

---
+++ src/main/java/net/imglib2/algorithm/blocks/dfield/Lookup2DProcessor.java
@@ -0,0 +1,17 @@
+package net.imglib2.algorithm.blocks.dfield;
+
+public // TODO: make package private again (public for testing)
+class Lookup2DProcessor< F, P > extends AbstractLookupProcessor< F, P >
+{
+	@Override
+	public AbstractLookupProcessor< F, P > independentCopy()
+	{
+		return null;
+	}
+
+	@Override
+	public void compute( final P src, final P dest )
+	{
+
+	}
+}
