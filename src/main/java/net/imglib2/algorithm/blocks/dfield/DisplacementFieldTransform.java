e5d1e829f14cb6bd2f42f9e3f06dc809c4461d41
tpietzsch <tobias.pietzsch@gmail.com>
2025-12-21 12:53:08 +0100

    Move createDisplacementFieldOperator to DisplacementFieldTransform

--- src/main/java/net/imglib2/algorithm/blocks/dfield/DisplacementFieldTransform.java
+++ src/main/java/net/imglib2/algorithm/blocks/dfield/DisplacementFieldTransform.java
@@ -33,17 +33,19 @@
  */
 package net.imglib2.algorithm.blocks.dfield;
 
-import java.util.function.Function;
+import static net.imglib2.type.PrimitiveType.FLOAT;
 
-import net.imglib2.algorithm.blocks.BlockProcessor;
 import net.imglib2.algorithm.blocks.BlockSupplier;
-import net.imglib2.algorithm.blocks.DefaultUnaryBlockOperator;
+import net.imglib2.algorithm.blocks.ClampType;
+import net.imglib2.algorithm.blocks.ComputationType;
 import net.imglib2.algorithm.blocks.UnaryBlockOperator;
-import net.imglib2.algorithm.blocks.transform.Transform;
+import net.imglib2.algorithm.blocks.transform.Transform.Interpolation;
 import net.imglib2.realtransform.AffineGet;
 import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.NativeType;
+import net.imglib2.type.PrimitiveType;
+import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
 
@@ -52,7 +54,6 @@ import net.imglib2.type.numeric.real.FloatType;
  */
 public class DisplacementFieldTransform
 {
-
 	/**
 	 * Interpolate and affine-transform blocks of the standard ImgLib2 {@code
 	 * RealType}s.
@@ -69,68 +70,113 @@ public class DisplacementFieldTransform
 	 *
 	 * @return factory for {@code UnaryBlockOperator} to affine-transform blocks of type {@code T}
 	 */
-	public static < T extends NativeType< T > >
-	Function< BlockSupplier< T >, UnaryBlockOperator< T, T > > affine( final AffineGet transformFromSource )
-	{
-		return s -> createAffineOperator( s.getType(), transformFromSource );
-	}
+//	public static < T extends NativeType< T > >
+//	Function< BlockSupplier< T >, UnaryBlockOperator< T, T > > affine( final AffineGet transformFromSource )
+//	{
+//		return s -> createAffineOperator( s.getType(), transformFromSource );
+//	}
 
 	/**
-	 * Create a {@code UnaryBlockOperator} to interpolate and affine-transform
-	 * blocks of the standard ImgLib2 {@code RealType}s.
-	 * <p>
-	 * Only 2D and 3D are supported currently!
-	 * <p>
-	 * {@code type} must be {@code DoubleType} of {@code FloatType}.
+	 * Combines an affine transformation into a (linearly interpolated)
+	 * displacement field, and look-up with resolting position field vectors in
+	 * a source image of type {@code T}, to produce an output image of type
+	 * {@code T}.
 	 *
 	 * @param type
-	 * 		instance of the input type
+	 * 		instance of the source/target type
 	 * @param transformFromSource
 	 * 		a 2D or 3D affine transform
+	 * @param displacementField
+	 *      the (normalized) displacement field
+	 * @param interpolation
+	 * 		which interpolation method to use for lookup in the source image.
+	 * 		(displacement field is always linearly interpolated)
+	 * @param computationType
+	 * 		For n-linear interpolation, this specifies in which precision
+	 * 		intermediate values should be computed. For {@code AUTO}, the type
+	 * 		that can represent the input/output type without loss of precision
+	 * 		is picked. That is, {@code FLOAT} for u8, i8, u16, i16, i32, f32,
+	 * 		and otherwise {@code DOUBLE} for u32, i64, f64. For nearest-neighbor
+	 * 		interpolation, {@code computationType} is not used.
+	 * @param clampType
+	 * 		For n-linear interpolation this specifies how interpolated values
+	 * 		(which are always float or double) should be clamped when converting
+	 * 		back to the source/target {@code type}. For nearest-neighbor
+	 * 		interpolation, {@code clampType} is not used.
+	 * @param <D>
+	 * 		displacement field type
 	 * @param <T>
-	 * 		the input/output type
-	 *
-	 * @return {@code UnaryBlockOperator} to affine-transform blocks of type {@code T}
+	 * 		the source/target type
 	 */
-	public static < T extends NativeType< T > >
-	UnaryBlockOperator< T, T > createAffineOperator( final T type, final AffineGet transformFromSource )
+	public static < D extends NativeType< D > & RealType< D >, T extends NativeType< T > >
+	UnaryBlockOperator< T, T > createDisplacementFieldOperator(
+			final T type,
+			final AffineGet transformFromSource,
+			final DisplacementField< D > displacementField,
+			final Interpolation interpolation,
+			final ComputationType computationType,
+			final ClampType clampType )
 	{
 		final int n = transformFromSource.numDimensions();
 		if ( n < 2 || n > 3 ) {
 			throw new IllegalArgumentException( "Only 2D and 3D affine transforms are supported currently" );
 		}
+		if ( displacementField.numDimensions() != n ) {
+			throw new IllegalArgumentException( "Number of dimension must be the same for the affine transform and the displacement field" );
+		}
 
-		if ( type instanceof FloatType || type instanceof DoubleType ) {
-			final AffineGet transformToSource = invert( transformFromSource );
-			return _affine( transformToSource, type );
-		} else {
-			throw new IllegalArgumentException( "Distance field must be DoubleType or FloatType" );
+		final AffineGet transformToSource = invert( transformFromSource );
+
+		if ( interpolation == Interpolation.NLINEAR )
+		{
+			final boolean processAsFloat;
+			switch ( computationType )
+			{
+			case FLOAT:
+				processAsFloat = true;
+				break;
+			case DOUBLE:
+				processAsFloat = false;
+				break;
+			default:
+			case AUTO:
+				final PrimitiveType pt = type.getNativeTypeFactory().getPrimitiveType();
+				processAsFloat = pt.equals( FLOAT ) || pt.getByteCount() < FLOAT.getByteCount();
+				break;
+			}
+			final UnaryBlockOperator< ?, ? > op = processAsFloat
+					? _disp( transformToSource, displacementField, interpolation, new FloatType() )
+					: _disp( transformToSource, displacementField, interpolation, new DoubleType() );
+			return op.adaptSourceType( type, ClampType.NONE ).adaptTargetType( type, clampType );
+		}
+		else // if ( interpolation == Interpolation.NEARESTNEIGHBOR )
+		{
+			return _disp( transformToSource, displacementField, interpolation, type );
 		}
 	}
 
-	private static < T extends NativeType< T > > UnaryBlockOperator< T, T > _affine( final AffineGet transform, final T type )
+	private static < D extends NativeType< D > & RealType< D >, T extends NativeType< T > > UnaryBlockOperator< T, T > _disp(
+			final AffineGet transform,
+			final DisplacementField< D > displacementField,
+			final Interpolation interpolation,
+			final T type )
 	{
 		final int n = transform.numDimensions();
-		final BlockProcessor< ?, ? > fieldProcessor;
-		if ( n == 2 )
-		{
-			fieldProcessor =
-					new DispFieldAffine2DProcessor<>( ( AffineTransform2D ) transform,
-							new double[] { 1, 1 }, Transform.Interpolation.NLINEAR,
-							type.getNativeTypeFactory().getPrimitiveType()
-					);
-		}
-		else
-		{
-			fieldProcessor =
-					new DispFieldAffine3DProcessor<>( ( AffineTransform3D ) transform,
-							new double[] { 1, 1 }, Transform.Interpolation.NLINEAR,
-							type.getNativeTypeFactory().getPrimitiveType()
-					);
-		}
-		return new DefaultUnaryBlockOperator<>( type, type, n + 1, n, fieldProcessor );
+		final PrimitiveType primitiveType = type.getNativeTypeFactory().getPrimitiveType();
+		final PrimitiveType dfieldPrimitiveType = displacementField.getType().getNativeTypeFactory().getPrimitiveType();
+		final double[] scale = displacementField.scale();
+		final BlockSupplier< D > displacements = displacementField.displacements();
+		final AbstractDispFieldAffineProcessor< ? > fieldProcessor = ( n == 2 )
+				? new DispFieldAffine2DProcessor<>( ( AffineTransform2D ) transform, scale, interpolation, dfieldPrimitiveType )
+				: new DispFieldAffine3DProcessor<>( ( AffineTransform3D ) transform, scale, interpolation, dfieldPrimitiveType );
+		final AbstractLookupProcessor< ?, ? > lookupProcessor = ( n == 2 )
+				? new Lookup2DProcessor<>( dfieldPrimitiveType, interpolation, primitiveType )
+				: new Lookup3DProcessor<>( dfieldPrimitiveType, interpolation, primitiveType );
+		return new DisplacementFieldUnaryBlockOperator<>( type, n, fieldProcessor, displacements, lookupProcessor );
 	}
 
+	// TODO: This same method is also in net.imglib2.algorithm.blocks.transform.Transform.
+	//       Should we make it public? Put it into a utility class?
 	private static AffineGet invert( final AffineGet transformFromSource )
 	{
 		switch ( transformFromSource.numDimensions() )
@@ -151,4 +197,5 @@ public class DisplacementFieldTransform
 			throw new IllegalArgumentException();
 		}
 	}
+
 }
