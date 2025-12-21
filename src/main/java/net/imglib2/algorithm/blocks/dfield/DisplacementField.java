9222d487c5036bbd441d6bd5ece99a576b16a35e
tpietzsch <tobias.pietzsch@gmail.com>
2025-12-21 12:24:22 +0100

    DisplacementField javadoc and getters

--- src/main/java/net/imglib2/algorithm/blocks/dfield/DisplacementField.java
+++ src/main/java/net/imglib2/algorithm/blocks/dfield/DisplacementField.java
@@ -18,25 +18,39 @@ import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
 
+/**
+ * A normalized displacement field, and its scale and offset with respect to a
+ * source image.
+ * <p>
+ * A "normalized" field expresses displacements in units relative to the
+ * displacement grid's own pixel spacing. Consequently, downsampling the grid by
+ * a factor of N requires scaling the displacement vectors by 1/N to maintain
+ * normalization.
+ *
+ * @param <T> displacement component type. should be {@code DoubleType} or {@code FloatType}
+ */
 public class DisplacementField< T extends NativeType< T > & RealType< T > >
 	implements EuclideanSpace, Typed< T >
 {
 	private final BlockSupplier< T > displacements;
 
-	private final double[] displacementScale;
+	private final double[] scale;
 
-	private final double[] displacementTranslation;
+	private final double[] translation;
 
 	public DisplacementField(
 			final BlockSupplier< T > displacements,
-			final double[] displacementScale,
-			final double[] displacementTranslation )
+			final double[] scale,
+			final double[] translation )
 	{
 		// TODO: verify that dimensionality of all arguments matches
+		if ( displacements.numDimensions() != scale.length || translation.length != scale.length ) {
+			throw new IllegalArgumentException( "Dimensionality of scale and translation must match the number of dimensions in the displacement field" );
+		}
 
 		this.displacements = displacements;
-		this.displacementScale = displacementScale;
-		this.displacementTranslation = displacementTranslation;
+		this.scale = scale;
+		this.translation = translation;
 
 	}
 
@@ -50,6 +64,51 @@ public class DisplacementField< T extends NativeType< T > & RealType< T > >
 	public T getType()
 	{
 		return displacements.getType();
+	}
+
+	/**
+	 * Displacement vector field.
+	 * <p>
+	 * The components of the displacement vector are in dimension 0, shifting
+	 * all other dimensions by 1. (The returned {@code BlockSupplier} has {@link
+	 * #numDimensions()} + 1 dimensions.)
+	 *
+	 * @return the displacement field
+	 */
+	public BlockSupplier< T > displacements()
+	{
+		return displacements;
+	}
+
+	/**
+	 * The {@link #displacements() displacement field} coordinates and
+	 * displacement vectors should be scaled by this factor when looking up
+	 * intensities in a source image.
+	 * <p>
+	 * In other words, this is the spacing of the displacement field grid in
+	 * units of source image pixels.
+	 *
+	 * @return relative scale of the displacement field wrt the source image
+	 */
+	public double[] scale()
+	{
+		return scale;
+	}
+
+	/**
+	 * When interpolating {@link #displacements()} into a position field for
+	 * value look-up in the source image, this translation vector is added.
+	 * (This happens after scaling, so the translation is in units of source
+	 * image pixels).
+	 * <p>
+	 * For example, this can be used to compensate for half-pixel offsets when
+	 * applying downsampled displacement fields to full-resolution images.
+	 *
+	 * @return position field offset in source pixels
+	 */
+	public double[] translation()
+	{
+		return translation;
 	}
 
 
@@ -133,7 +192,7 @@ public class DisplacementField< T extends NativeType< T > & RealType< T > >
 		final int n = transform.numDimensions();
 		final PrimitiveType primitiveType = type.getNativeTypeFactory().getPrimitiveType();
 		final PrimitiveType dfieldPrimitiveType = displacementField.getType().getNativeTypeFactory().getPrimitiveType();
-		final double[] scale = displacementField.displacementScale;
+		final double[] scale = displacementField.scale;
 		final BlockSupplier< D > displacements = displacementField.displacements;
 		final AbstractDispFieldAffineProcessor< ? > fieldProcessor = ( n == 2 )
 				? new DispFieldAffine2DProcessor<>( ( AffineTransform2D ) transform, scale, interpolation, dfieldPrimitiveType )
